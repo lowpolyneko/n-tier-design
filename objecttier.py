37b8b7b1463b627af80cf0d0140863308e61adc1
Neko <lowpolyneko@protonmail.ch>
2024-09-23 17:23:12 -0500

    finish hw

--- objecttier.py
+++ objecttier.py
@@ -22,7 +22,27 @@ import datatier
 #   Percent_Ridership: float
 #
 class Station:
-   pass
+    def __init__(self, Station_ID: int, Station_Name: str, Ridership: int, Percent_Ridership: float) -> None:
+        self.__Station_ID = Station_ID
+        self.__Station_Name = Station_Name
+        self.__Ridership = Ridership
+        self.__Percent_Ridership = Percent_Ridership
+
+    @property
+    def Station_ID(self) -> int:
+        return self.__Station_ID
+
+    @property
+    def Station_Name(self) -> str:
+        return self.__Station_Name
+
+    @property
+    def Ridership(self) -> int:
+        return self.__Ridership
+
+    @property
+    def Percent_Ridership(self) -> float:
+        return self.__Percent_Ridership
 
 
 ########################################################
@@ -40,8 +60,42 @@ class Station:
 #   Lines: list of strings
 #
 class Stop:
-   pass
+    def __init__(self, Stop_ID: int, Stop_Name: str, Direction: str, Accessible: bool, Latitude: float, Longitude: float, Lines: list[str]) -> None:
+        self.__Stop_ID = Stop_ID
+        self.__Stop_Name = Stop_Name
+        self.__Direction = Direction
+        self.__Accessible = Accessible
+        self.__Latitude = Latitude
+        self.__Longitude = Longitude
+        self.__Lines = Lines
 
+    @property
+    def Stop_ID(self) -> int:
+        return self.__Stop_ID
+
+    @property
+    def Stop_Name(self) -> str:
+        return self.__Stop_Name
+
+    @property
+    def Direction(self) -> str:
+        return self.__Direction
+
+    @property
+    def Accessible(self) -> bool:
+        return self.__Accessible
+
+    @property
+    def Latitude(self) -> float:
+        return self.__Latitude
+
+    @property
+    def Longitude(self) -> float:
+        return self.__Longitude
+
+    @property
+    def Lines(self) -> list[str]:
+        return self.__Lines
 
 ########################################################
 #
@@ -55,7 +109,26 @@ class Stop:
 #          returns None if an error occurs.
 #
 def get_stations(dbConn, pattern):
-   pass
+    entries = datatier.select_n_rows(dbConn, f"""
+        SELECT Station_Name, Stations.Station_ID, SUM(Num_Riders) FROM Stations
+        JOIN Ridership ON Stations.Station_ID = Ridership.Station_ID
+        WHERE Station_Name LIKE "{pattern}"
+        GROUP BY Stations.Station_ID
+        ORDER BY Station_Name
+    """)
+
+    if not entries:
+        return None
+
+    total, = datatier.select_one_row(dbConn, """
+        SELECT SUM(Num_Riders) FROM Ridership
+    """)
+
+    results = []
+    for Station_Name, Station_ID, Num_Riders in entries:
+        results.append(Station(Station_Name, Station_ID, Num_Riders, 100*Num_Riders/total))
+
+    return results
 
 
 ########################################################
@@ -72,4 +145,34 @@ def get_stations(dbConn, pattern):
 #          occurs.
 #
 def get_stops(dbConn, name):
-   pass
+    station = datatier.select_one_row(dbConn, """
+        SELECT Station_ID FROM Stations
+        WHERE Station_Name = ?
+    """, [name])
+
+    if not station:
+        return []
+
+    station_id, = station
+
+    stops = datatier.select_n_rows(dbConn, """
+        SELECT Stop_ID, Stop_Name, Direction, ADA, Latitude, Longitude
+        FROM Stops
+        WHERE Station_ID = ?
+        ORDER BY Stop_ID
+    """, [station_id])
+
+    results = []
+    for Stop_ID, Stop_Name, Direction, ADA, Latitude, Longitude in stops:
+        # get lines
+        lines = datatier.select_n_rows(dbConn, """
+            SELECT Color FROM Lines
+            JOIN StopDetails ON Lines.Line_ID = StopDetails.Line_ID
+            JOIN Stops ON StopDetails.Stop_ID = Stops.Stop_ID
+            WHERE StopDetails.Stop_ID = ?
+            ORDER BY Color
+        """, [Stop_ID])
+
+        results.append(Stop(Stop_ID, Stop_Name, Direction, ADA, Latitude, Longitude, [x for x, in lines]))
+
+    return results
